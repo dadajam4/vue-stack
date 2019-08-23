03c6925f1d7ef57e2a6556a89dd9a09550d76194
dadajam4 <dadajam4@gmail.com>
2019-08-23 17:02:50 +0900

    fix: disable document overflow

--- src/lib/directives/body-scroll-lock.ts
+++ src/lib/directives/body-scroll-lock.ts
@@ -5,22 +5,65 @@ export interface ClickOutsideDirective extends VNodeDirective {
   value?: boolean;
 }
 
+class Stacks {
+  els: HTMLElement[] = [];
+  active: boolean = false;
+
+  private check() {
+    const active = this.els.length > 0;
+    if (active && !this.active) {
+      this.activate();
+    } else if (!active && this.active) {
+      this.deactivate();
+    }
+  }
+
+  private activate() {
+    document.documentElement.setAttribute('data-vstack-body-scroll-lock', '');
+    this.active = true;
+  }
+
+  private deactivate() {
+    document.documentElement.removeAttribute('data-vstack-body-scroll-lock');
+    this.active = false;
+  }
+
+  push(el: HTMLElement) {
+    if (!this.els.includes(el)) {
+      disableBodyScroll(el);
+      this.els.push(el);
+      this.check();
+    }
+  }
+
+  remove(el: HTMLElement) {
+    const index = this.els.indexOf(el);
+    if (index !== -1) {
+      enableBodyScroll(el);
+      this.els.splice(index, 1);
+      this.check();
+    }
+  }
+}
+
+const stacks = new Stacks();
+
 export default {
   name: 'body-scroll-lock',
 
   inserted(el: HTMLElement, binding: ClickOutsideDirective) {
-    binding.value && disableBodyScroll(el);
+    binding.value && stacks.push(el);
   },
 
   componentUpdated(el, binding: ClickOutsideDirective) {
     if (binding.value) {
-      disableBodyScroll(el);
+      stacks.push(el);
     } else {
-      enableBodyScroll(el);
+      stacks.remove(el);
     }
   },
 
   unbind(el: HTMLElement) {
-    enableBodyScroll(el);
+    stacks.remove(el);
   },
 };
